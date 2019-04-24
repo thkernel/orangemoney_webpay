ea6250883d214f864789c36fb5e2aff210edcdee
thkernel <salut.amos@gmail.com>
2019-04-24 19:55:59 +0000

    first commit

---
+++ lib/orangemali_sms.rb
@@ -0,0 +1,6 @@
+require "orangemali_sms/version"
+
+module OrangemaliSms
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/orangemali_sms/version.rb
@@ -0,0 +1,3 @@
+module OrangemaliSms
+  VERSION = "0.1.0"
+end
