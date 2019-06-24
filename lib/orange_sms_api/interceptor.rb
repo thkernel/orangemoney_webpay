cf75dd810de393cb54e32fe33de5819dca99a9ef
thkernel <salut.amos@gmail.com>
2019-06-24 14:44:05 +0000

    update

--- lib/orange_sms_api/interceptor.rb
+++ lib/orange_sms_api/interceptor.rb
@@ -32,7 +32,7 @@ module OrangeSmsApi
             OrangeSmsApi.configure do |config|
                 config.access_token = response_body.access_token
             end
-            puts "LE TOKEN: #{OrangeSmsApi.configuration.access_token}"
+            puts "LE TOKEN: #{OrangeSmsApi.configuration.access_token.to_s}"
            else
             puts "RESPONSE STATUT: #{response.status}"
 
